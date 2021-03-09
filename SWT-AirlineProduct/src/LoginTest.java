ab4613a0f0463f550634d459b7c79968f61d5064
MontoyaR <42822862+MontoyaR@users.noreply.github.com>
2021-03-09 09:51:13 -0500

    Added method comments

--- SWT-AirlineProduct/src/LoginTest.java
+++ SWT-AirlineProduct/src/LoginTest.java
@@ -1,15 +1,21 @@
-import java.awt.event.ActionEvent;
-import org.junit.jupiter.api.AfterEach;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Class used to perform testing on the Login.java class.
+ */
 public class LoginTest {
   private Login login;
   private User user;
 
+  /**
+   * The code within @setUp is run before each test method execution. An object of class User is
+   * created so that the expected and actual are compared.
+   */
   @BeforeEach
   public void setUp() {
     login = new Login();
@@ -17,17 +23,53 @@ public class LoginTest {
 
   }
 
+  /**
+   * Test Case ID: NegTest-LoginCase1
+   * Requirement ID/Description: REQ-10 The system shall allow users to log in to the system if they
+   *                             provide the correct credentials (Username & Password).
+   * Purpose: To test that a user can access the home page by ensuring that both username and
+   *          password are valid.
+   * Test setup: An object of the User class is created with the username='rjumar' and password='123'
+   * Test Strategy: Decision Table Testing
+   * ID	          Condition/Action                  Test Case 1	Test Case 2	Test Case 3	Test Case 4
+   * Condition 1	Valid Username	                       T	        T	            F	        F
+   * Condition 2	Valid Password	                       T	        F	            T	        F
+   * Action 1	    Home Page	                           Execute
+   * Action 2   	Show a message displaying
+   *              'Username or password do not match.'	 	          Execute	     Execute	  Execute
+   * Input: Call method handleLoginEnter(null, "rjumar", "123")
+   * Expected Output: Method handleLoginEnter return an object of User with username='rjumar' and
+   *                  password='123'
+   */
   @Test
   @DisplayName("User login credentials shall allow access to the Airline Reservation System.")
   void handleValidLogin() {
     User result = login.handleLoginEnter(null, "rjumar", "123");
 
-    assertEquals("rjumar", result.getUsername(), "Username matches.");
-    assertEquals("123", result.getPassword(), "Password matches.");
+    assertEquals(user.getUsername(), result.getUsername(), "Username matches.");
+    assertEquals(user.getPassword(), result.getPassword(), "Password matches.");
   }
 
+  /**
+   * Test Case ID: NegTest-LoginCase4
+   * Requirement ID/Description: REQ-10 The system shall allow users to log in to the system if they
+   *                             provide the correct credentials (Username & Password).
+   * Purpose: To test that the system performs correct exception handling by not allowing a user to
+   *          access the home page if username and password is incorrect.
+   * Test setup: An object of the User class is created with the username='rjumar' and password='123'
+   * Test Strategy: Decision Table Testing
+   * ID	          Condition/Action                  Test Case 1	Test Case 2	Test Case 3	Test Case 4
+   * Condition 1	Valid Username	                       T	        T	            F	        F
+   * Condition 2	Valid Password	                       T	        F	            T	        F
+   * Action 1	    Home Page	                           Execute
+   * Action 2   	Show a message displaying
+   *              'Username or password do not match.'	 	          Execute	     Execute	  Execute
+   * Input: Call method handleLoginEnter(null, "", "")
+   * Expected Output: Method handleLoginEnter returns an exception message 'Username or Password
+   *                  cannot be blank.'
+   */
   @Test
-  @DisplayName("User login should be invalid due to an empty login or username.")
+  @DisplayName("User login should be invalid due to an invalid login and username.")
   void handleLoginEmpty() {
     Exception exception =
         assertThrows(ArithmeticException.class, () -> login.handleLoginEnter(null, "",
@@ -35,6 +77,24 @@ public class LoginTest {
     assertEquals("Username or Password cannot be blank.", exception.getMessage());
   }
 
+  /**
+   * Test Case ID: NegTest-LoginCase3
+   * Requirement ID/Description: REQ-10 The system shall allow users to log in to the system if they
+   *                             provide the correct credentials (Username & Password).
+   * Purpose: To test that the system performs correct exception handling by not allowing a user to
+   *          access the home page if username is incorrect.
+   * Test setup: An object of the User class is created with the username='rjumar' and password='123'
+   * Test Strategy: Decision Table Testing
+   * ID	          Condition/Action                  Test Case 1	Test Case 2	Test Case 3	Test Case 4
+   * Condition 1	Valid Username	                       T	        T	            F	        F
+   * Condition 2	Valid Password	                       T	        F	            T	        F
+   * Action 1	    Home Page	                           Execute
+   * Action 2   	Show a message displaying
+   *              'Username or password do not match.'	 	          Execute	     Execute	  Execute
+   * Input: Call method handleLoginEnter(null, "rjumarr", "123")
+   * Expected Output: Method handleLoginEnter returns an exception message 'Username or Password
+   *                  do not match.'
+   */
   @Test
   @DisplayName("User login should be invalid due to an incorrect username.")
   void handleInvalidUsernameLogin() {
@@ -44,6 +104,24 @@ public class LoginTest {
     assertEquals("Username or password do not match.", exception.getMessage());
   }
 
+  /**
+   * Test Case ID: NegTest-LoginCase2
+   * Requirement ID/Description: REQ-10 The system shall allow users to log in to the system if they
+   *                             provide the correct credentials (Username & Password).
+   * Purpose: To test that the system performs correct exception handling by not allowing a user to
+   *          access the home page if password is incorrect.
+   * Test setup: An object of the User class is created with the username='rjumar' and password='123'
+   * Test Strategy: Decision Table Testing
+   * ID	          Condition/Action                  Test Case 1	Test Case 2	Test Case 3	Test Case 4
+   * Condition 1	Valid Username	                       T	        T	            F	        F
+   * Condition 2	Valid Password	                       T	        F	            T	        F
+   * Action 1	    Home Page	                           Execute
+   * Action 2   	Show a message displaying
+   *              'Username or password do not match.'	 	          Execute	     Execute	  Execute
+   * Input: Call method handleLoginEnter(null, "rjumar", "1234")
+   * Expected Output: Method handleLoginEnter returns an exception message 'Username or Password
+   *                  do not match.'
+   */
   @Test
   @DisplayName("User login should be invalid due to an incorrect password.")
   void handleInvalidPasswordLogin() {
@@ -52,5 +130,4 @@ public class LoginTest {
             "1234"));
     assertEquals("Username or password do not match.", exception.getMessage());
   }
-
 }
