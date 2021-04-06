397b510aa1673fae63f32923026ad2def46de1ac
anagalacticRuby <841b2dRuby>
2021-04-06 02:12:56 -0400

    Modified pom.xml to support mockito-junit optimized some imports

--- SWT-AirlineProduct/src/UserCreationIntegrationTest.java
+++ SWT-AirlineProduct/src/UserCreationIntegrationTest.java
@@ -5,18 +5,20 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.mockito.stubbing.OngoingStubbing;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class UserCreationIntegrationTest {
 
   private User dummyUser;
