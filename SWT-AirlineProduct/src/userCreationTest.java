80198d71784d9c2457b27dc51974e4d1dabb7dae
MontoyaR <42822862+MontoyaR@users.noreply.github.com>
2021-04-20 10:27:54 -0400

    Fixed test cases add comments

--- SWT-AirlineProduct/src/userCreationTest.java
+++ SWT-AirlineProduct/src/userCreationTest.java
@@ -25,6 +25,10 @@ public class userCreationTest {
     dummyUser = new User("UO004", "Ricky", "Jumar", "rjumar", "1234");
   }
 
+  /**
+   * The code within @AfterEach is run after each test method execution. It is so that the database
+   * returns to it original state after each user creation.
+   */
   @AfterEach
   public void tearDown() {
     Connection con = DBUtil.dbConnect();
@@ -47,8 +51,23 @@ public class userCreationTest {
   }
 
   /**
-   * Test Case ID: Requirement: #4,The system shall require that every field is filled before
-   * processing requests.
+   * Test Case ID: Requirement: REQ-4 The system shall require that every field is filled before
+   *               processing requests.
+   * Purpose: The purpose is to test when a user has left an empty field on user creation and
+   *          shows the response from the system.
+   * Test Setup: A dummy object of type User is declared within the setup method with the following
+   *             data: User("UO004", "Ricky", "Jumar", "rjumar", "1234"). Within emptyUserFieldTest,
+   *             the username is set to "" so that it denotes an empty field.
+   * Test Strategy: Decision Table Testing
+   * ID	            Condition/Action                    Test Case 1
+   * Condition 1	  Valid Username	                        F
+   * Condition 2	  Valid Password	                        T
+   * Action 1	      User Created
+   * Action 2   	  Show a message displaying            Execute
+   *                "Username or password is blank"
+   * Input: Call to method createUser with dummyUser attributes and a modified username.
+   * Expected Output: Method call createUser returns an exception message 'Username or Password
+   *                  cannot be blank.'
    */
   @Test
   @DisplayName("System shall prevent user accounts from having empty fields")
@@ -61,15 +80,22 @@ public class userCreationTest {
   }
 
   /**
-   * Test Case ID: Requirement ID/Description: REQ-5 The system shall require the user to create a
-   * password for their profile. Purpose: Test setup: An object of the User class is created with
-   * the id='CS001', firstname='john', lastname='Alex', nic=34232222, passport=3443, address=Uk,
-   * dob='null', gender='Male', contact=3432434, photo='null' Test Strategy: ID Condition/Action
-   * Test Case 1 Test Case 2 Condition 1 Valid Username T T Condition 2 Valid Password T F Action 1
-   * User Created Execute Action 2 Show a message displaying 'Username or password cannot' Execute
-   * be blank Input: Call method handleLoginEnter(null, "rjumar", "321", "Ravi", "Kumar", "UO004")
-   * Expected Output: Method handleCreation return a object of User with password='321' that results
-   * in a successful user creation.
+   * Test Case ID:
+   * Requirement ID/Description: REQ-3 The system shall allow users to create user accounts for other users.
+   * Purpose: The purpose is to test the creation of a user with valid credentials.
+   * Test Setup: A dummy object of type User is declared within the setup method with the following
+   *             data: User("UO004", "Ricky", "Jumar", "rjumar", "1234"). A result object of class User is
+   *             returned after insertion to the database and the password attributes are compared.
+   * Test Strategy: Decision Table Testing
+   * ID	            Condition/Action                    Test Case 1
+   * Condition 1	  Valid Username	                        T
+   * Condition 2	  Valid Password	                        T
+   * Action 1	      User Created                         Execute
+   * Action 2   	  Show a message displaying
+   *                "Username or password is blank"
+   * Input: Call to method createUser with dummyUser attributes.
+   * Expected Output: Method call createUser returns an object of type User to be compared and display
+   *                  message, "User Created.", is display to confirm the creation.
    */
   @Test
   @DisplayName("User creation should be valid due to an full password.")
@@ -80,20 +106,29 @@ public class userCreationTest {
   }
 
   /**
-   * Test Case ID: Requirement ID/Description: #16, The system shall require that every username be
-   * distinct from one another.
-   * This test class uses negative testing to validate that the exception is caught by a handler
-   * and therefore will prevent users from having duplicate usernames.
+   * Test Case ID:
+   * Requirement ID/Description: REQ-16 The system shall require that every username be distinct
+   *                             from one another.
+   * Purpose: The purpose is to test the system for exception handling when the user enters a username
+   *          that current exists in the database.
+   * Test Setup: A dummy object of type User is declared within the setup method with the following
+   *             data: User("UO004", "Ricky", "Jumar", "rjumar", "1234"). The dummy User object is then
+   *             created and stored to the database before the second user creation.
+   * Test Strategy: Negative Testing
+   * Input: Initial call to method createUser with dummyUser attributes. Second call to createUser
+   *        with the same username attribute.
+   * Expected Output: Method call createUser throws an exception: "Username cannot be a duplicate."
+   *                  to prevent duplication of usernames.
    */
   @Test
   @DisplayName("System shall prevent duplicate usernames from existing")
   void handleDuplicateUsername() {
+    userCreation.createUser(dummyUser);
+
     Exception exception =
         assertThrows(IllegalArgumentException.class, () -> userCreation.createUser(dummyUser));
     assertEquals("Username cannot be a duplicate.", exception.getMessage());
   }
-
-
 
   /**
    * Test Case ID: Requirement ID/Description: REQ-9 The system shall save all user profiles in a
