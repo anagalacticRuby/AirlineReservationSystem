da79ecfba423d0bbf7cb1024c1e27720a942959f
ZStrikeXIII <43644454+ZStrikeXIII@users.noreply.github.com>
2021-04-22 20:43:17 -0400

    Update MainIntegrationTest.java

--- SWT-AirlineProduct/Test/Integration/MainIntegrationTest.java
+++ SWT-AirlineProduct/Test/Integration/MainIntegrationTest.java
@@ -9,6 +9,17 @@ public class MainIntegrationTest {
 
   Main main;
 
+  /**
+   * Test Case ID: IntegrationTest-Main-001
+   * Requirement ID/Description: N/A
+   * Purpose: To test if the DBUtil connection is functioning correctly.
+   * Test Setup: We make a mock connection and a mock statement to simulate the DBUtil
+   * connection being used with invalid authentication.
+   * Test Strategy: An Integration Test where we use the mocks to confirm if the connection is
+   * working as expected.
+   * Input: A mock connection and a mock statement.
+   * Expected Output: An exception thrown due to invalid authentication.
+   */
   @Test
   @DisplayName("Mock for the invalid authentication functionality.")
   public void handleInvalidAuthentication() {
@@ -21,6 +32,18 @@ public class MainIntegrationTest {
     assertEquals(main.getLoginFlag(), false);
   }
 
+  /**
+   * Test Case ID: IntegrationTest-Main-002
+   * Requirement ID/Description: N/A
+   * Purpose: To test if the DBUtil connection is functioning correctly.
+   * Test Setup: We make a mock connection and a mock statement to simulate the DBUtil
+   * connection being used with valid authentication.
+   * Test Strategy: An Integration Test where we use the mocks to confirm if the connection is
+   * working as expected.
+   * Input: A mock connection and a mock statement.
+   * Expected Output: Confirmation that the DBUtil connection was successful and authentication
+   * was valid.
+   */
   @Test
   @DisplayName("Mock for the valid authentication functionality.")
   public void handleValidAuthentication() {
