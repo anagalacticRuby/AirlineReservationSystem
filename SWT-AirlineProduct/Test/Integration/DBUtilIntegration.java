9a99f495af7ca26dd7daf23a33554505b2ff0c91
ZStrikeXIII <43644454+ZStrikeXIII@users.noreply.github.com>
2021-04-22 20:41:24 -0400

    Update DBUtilIntegration.java

--- SWT-AirlineProduct/Test/Integration/DBUtilIntegration.java
+++ SWT-AirlineProduct/Test/Integration/DBUtilIntegration.java
@@ -42,6 +42,16 @@ public class DBUtilIntegration {
   }
 
 
+  /**
+   * Test Case ID: IntegrationTest-DB-001
+   * Requirement ID/Description: N/A
+   * Purpose: To test that the system will close database connections when needed.
+   * Test Setup: We feed the program a mock statement that we want to close the connection for.
+   * Test Strategy: An Integration Test where we run the test with a mock statement and observe
+   * how the program reacts.
+   * Input: A mock prepared statement with an open connection.
+   * Expected Output: Confirmation that the connection for the statement has closed.
+   */
   @Test
   @DisplayName("Mock for closing prepare statement functionality.")
   public void closeStatement() throws SQLException {
@@ -50,6 +60,16 @@ public class DBUtilIntegration {
     assertTrue(statement.isClosed());
   }
 
+  /**
+   * Test Case ID: IntegrationTest-DB-002
+   * Requirement ID/Description: N/A
+   * Purpose: To test that the system will close the result set when needed.
+   * Test Setup: We feed a mock result set into the program we want it to close.
+   * Test Strategy: An Integration Test where we use a mock result set and observe how the system
+   * reacts.
+   * Input: A mock result set.
+   * Expected Output: Confirmation that the result set is closed.
+   */
   @Test
   @DisplayName("Mock for closing the resultSet.")
   public void closeResultSet() throws SQLException {
@@ -58,7 +78,17 @@ public class DBUtilIntegration {
     assertEquals(false, resultSet.isClosed());
   }
 
-
+  /**
+   * Test Case ID: IntegrationTest-DB-003
+   * Requirement ID/Description: N/A
+   * Purpose: To test if the DBUtil connection is functioning correctly.
+   * Test Setup: We make a mock connection and a mock statement to simulate the DBUtil
+   * connection being used.
+   * Test Strategy: An Integration Test where we use the mocks to confirm if the connection is
+   * working as expected.
+   * Input: A mock connection and a mock statement.
+   * Expected Output: Confirmation that the DBUtil connection was successful.
+   */
   @Test
   @DisplayName("The constant connection to the database will be mocked.")
   public void mockDBUtilConnection() throws Exception {
