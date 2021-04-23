cac4db9d9399cd5a468e40f0a49d333b37a89b15
ZStrikeXIII <43644454+ZStrikeXIII@users.noreply.github.com>
2021-04-22 20:56:33 -0400

    Update ticketUnitTest.java

--- SWT-AirlineProduct/Test/Unit/ticketUnitTest.java
+++ SWT-AirlineProduct/Test/Unit/ticketUnitTest.java
@@ -6,6 +6,18 @@ import org.junit.jupiter.api.Test;
 
 public class ticketUnitTest {
 	
+	/**
+	 * Test Case ID: UnitTest-Ticket-001
+	 * Requirement ID/Description: REQ-11 The system shall allow a user to view every ticket entry
+	 * stored in the database.
+	 * Purpose: To ensure we can search for a ticket within the database, specifically in this test
+	 * case by source and departure locations.
+	 * Test Setup: A new ticket with specified source and departure locations we can search for.
+	 * Test Strategy: A Unit Test with Input Validation to verify that the ticket has been
+	 * found successfully.
+	 * Input: A new ticket with specified source and departure locations.
+	 * Expected Output: Verification that the ticket has been found.
+	 */
 	@Test
 	@DisplayName("Test to ensure search functionality.")
 	public void testSearchTicket() {
