6ace35b87c154c40b1fc6a84e9e04001327a62a6
ZStrikeXIII <43644454+ZStrikeXIII@users.noreply.github.com>
2021-04-22 20:37:25 -0400

    Update UserCreationGUI.java

--- SWT-AirlineProduct/Test/GUI/UserCreationGUI.java
+++ SWT-AirlineProduct/Test/GUI/UserCreationGUI.java
@@ -28,6 +28,19 @@ public class UserCreationGUI extends AssertJSwingJUnitTestCase {
     frameFixture.show();
   }
 
+  /**
+   * Test Case ID: IntegrationTest-UserCreate-001
+   * Requirement ID/Description: REQ-4 The system shall require that every field is filled
+   * before processing requests.
+   * Purpose: To ensure that a user can be created within the database, and the GUI reacts
+   * accordingly.
+   * Test Setup: We input a new user with all the needed information.
+   * Test Strategy: A GUI Test where we determine if the GUI reacts accordingly when the specified
+   * process is executed.
+   * Input: A new user with all of the specified fields.
+   * Expected Output: Confirmation that the new user has been added, and the GUI reacting
+   * as it should.
+   */
   @Test
   @DisplayName("GUI Testing for user creation.")
   public void handleValidUserCreation() {
@@ -44,6 +57,20 @@ public class UserCreationGUI extends AssertJSwingJUnitTestCase {
     frameFixture.cleanUp();
   }
 
+  /**
+   * Test Case ID: IntegrationTest-UserCreate-002
+   * Requirement ID/Description: REQ-16 The system shall require that every username be distinct
+   * from one another.
+   * Purpose: To ensure that a new user with a username that already exists is not allowed by the
+   * system, and the GUI react accordingly.
+   * Test Setup: We will create a new user, but with a username that is already within the
+   * database.
+   * Test Strategy: A GUI Test where we determine if the GUI reacts accordingly when the specified
+   * process is executed.
+   * Input: A new user with a username that has already been entered into the database.
+   * Expected Output: An exception telling the user that duplicate usernames are not allowed, and
+   * the GUI reacting as it should.
+   */
   @Test
   @DisplayName("GUI Testing for duplicate user creation.")
   public void handleDuplicateUserCreation() {
