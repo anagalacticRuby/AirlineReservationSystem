abe92e573022764e08ddab8085ffccae936bd9f1
ZStrikeXIII <43644454+ZStrikeXIII@users.noreply.github.com>
2021-04-22 20:55:59 -0400

    Update addCustomerTest.java

--- SWT-AirlineProduct/Test/Unit/addCustomerTest.java
+++ SWT-AirlineProduct/Test/Unit/addCustomerTest.java
@@ -7,6 +7,16 @@ import static org.junit.Assert.*;
 
 public class addCustomerTest {
 
+	/**
+	 * Test Case ID: UnitTest-AddCustomer-001
+	 * Requirement ID/Description: REQ-29: The user shall be able to add customers.
+	 * Purpose: To test if the user will be able to add a customer into the system successfully.
+	 * Test Setup: The user creates a customer with the information required by the system.
+	 * Test Strategy: We utilize a unit test that will confirm a customer has been created and
+	 * added into the system successfully.
+	 * Input: A new customer inputted by the user with the needed information.
+	 * Expected Output: The system will confirm a customer has been added.
+	 */
 	@Test
 	@DisplayName("Customer creation should be executed.")
 	public void testAddCustomer() {
